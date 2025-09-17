8a02e905db1b5cc6c6d34bc5cd0ca2f34899cbde
SketchMorph <msp3692a@gmail.com>
2025-09-17 11:06:32 +0900

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,8 @@
+// hanbok-site/next-i18next.config.js
 module.exports = {
   i18n: {
     defaultLocale: "ko",
-    locales: ["ko", "en", "ja", "zh", "fr"],
+    locales: ["ko", "en", "ja", "zh", "fr"]
   },
+  reloadOnPrerender: true // 개발환경에서 Hot Reload 반영
 };
